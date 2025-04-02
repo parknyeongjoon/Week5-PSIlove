26ae41e8312d09ac1d3bbdc6c029fdff117733c3
Mstone8370 <mstone8370@gmail.com>
2025-04-02 11:49:28 +0900

    first

---
+++ Engine/Engine/Source/Runtime/Launch/EngineLoop.h
@@ -0,0 +1,54 @@
+#pragma once
+#include "Core/HAL/PlatformType.h"
+#include "D3D11RHI/GraphicDevice.h"
+#include "Renderer/Renderer.h"
+#include "Engine/ResourceMgr.h"
+
+class UnrealEd;
+class UImGuiManager;
+class UWorld;
+class FEditorViewportClient;
+class SSplitterV;
+class SSplitterH;
+class SLevelEditor;
+
+class FEngineLoop
+{
+public:
+    FEngineLoop();
+
+    int32 PreInit();
+    int32 Init(HINSTANCE hInstance);
+    void Render();
+    void Tick();
+    void Exit();
+    float GetAspectRatio(IDXGISwapChain* swapChain) const;
+    void Input();
+
+private:
+    void WindowInit(HINSTANCE hInstance);
+
+public:
+    static FGraphicsDevice graphicDevice;
+    static FRenderer renderer;
+    static FResourceMgr resourceMgr;
+    static uint32 TotalAllocationBytes;
+    static uint32 TotalAllocationCount;
+
+
+    HWND hWnd;
+
+private:
+    UImGuiManager* UIMgr;
+    UWorld* GWorld;
+    SLevelEditor* LevelEditor;
+    UnrealEd* UnrealEditor;
+    bool bIsExit = false;
+    const int32 targetFPS = 60;
+    bool bTestInput = false;
+
+public:
+    UWorld* GetWorld() const { return GWorld; }
+    SLevelEditor* GetLevelEditor() const { return LevelEditor; }
+    UnrealEd* GetUnrealEditor() const { return UnrealEditor; }
+};
