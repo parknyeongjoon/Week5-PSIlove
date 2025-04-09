0c32f7c64cc639ec4eddd1a1fb8cf89fe469729d
Kyeongtaek Lee <skylove0911@naver.com>
2025-04-09 23:08:17 +0900

    [fix] merge conflict

--- Engine/Engine/Source/Runtime/Engine/Level.cpp
+++ Engine/Engine/Source/Runtime/Engine/Level.cpp
@@ -19,8 +19,8 @@ void ULevel::Initialize(EWorldType worldType)
     //SpawnObject(OBJ_CUBE);
     FManagerOBJ::CreateStaticMesh("Assets/Dodge/Dodge.obj");
     FManagerOBJ::CreateStaticMesh("Assets/SkySphere.obj");
-    //FManagerOBJ::CreateStaticMesh("Assets/RoadCube.obj");
-    //FManagerOBJ::CreateStaticMesh("Assets/Demo.obj");
+    FManagerOBJ::CreateStaticMesh("Assets/RoadCube.obj");
+    FManagerOBJ::CreateStaticMesh("Assets/Demo.obj");
     
     Fog = SpawnActor<AFog>();
 }
